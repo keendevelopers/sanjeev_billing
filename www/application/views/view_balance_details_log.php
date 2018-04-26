496a6f600c8def18a2b2b915852b33788342e28f
unknown <pawan55292@gmail.com>
2018-04-26 21:03:46 +0530

    table changes

--- www/application/views/view_balance_details_log.php
+++ www/application/views/view_balance_details_log.php
@@ -5,18 +5,16 @@ $bill          = $this->db->where(array('bill_id' => $parm,'ledger_type' => 'buy
 // echo $this->db->last_query();die();
 
 // To Get Last Entry of this bill id
-// $last_entery  = end($bill);
+$last_entery  = end($bill);
 
 // echo '<pre>';
     // print_r($last_entery);
     // echo '</pre>';die();
  ?>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-<link rel="stylesheet" href="https://cdn.datatables.net/1.10.16/css/dataTables.bootstrap.min.css">
-
+ 
 <div class="col-xs-12 col-md-12 col-lg-12 pull-left">
-<!--  -->
-				<!-- <form id="pay_remain_balance" method="post" action="<?php echo base_url("product/add_remain_balance"); ?>">
+
+				 <form id="pay_remain_balance" method="post" action="<?php echo base_url("product/add_remain_balance"); ?>">
                     <div class="panel panel-default height">
 
                         <div class="panel-heading text-center">Total Bill Record</div>
@@ -27,51 +25,13 @@ $bill          = $this->db->where(array('bill_id' => $parm,'ledger_type' => 'buy
 
                              <div class="col-md-5"><h4><b>Bill ID:</b></h4></div><div class="col-md-7"><h5><input type="text"  class="form-control required" name="bill_id" id="bill_id"  value="<?php echo $last_entery['bill_id']; ?>" readonly /></h5></div></div>
 
-                           <div class="row"><div class="col-md-5"><h4><b>Total Amount:</b></h4></div><div class="col-md-7"><h4><input type="text"  class="form-control required" name="total_amount" id="total_amount"  value="<?php echo $last_entery['total_amount']; ?>" readonly /></h4></div></div> -->
+                           <div class="row"><div class="col-md-5"><h4><b>Total Amount:</b></h4></div><div class="col-md-7"><h4><input type="text"  class="form-control required" name="total_amount" id="total_amount"  value="<?php echo $last_entery['total_amount']; ?>" readonly /></h4></div></div>
 
-						   <!-- Table Starts From here -->
-                           <div class="row card-box">
-                           <div class="col-md-12">
-                           <!--  -->
-              
-                             <table id="example" class="display" style="width:100%">
-        <thead>
-            <tr>
-                <th>Name</th>
-                <th>Position</th>
-                <th>Office</th>
-                <th>Age</th>
-                <th>Start date</th>
-                <th>Salary</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>Tiger Nixon</td>
-                <td>System Architect</td>
-                <td>Edinburgh</td>
-                <td>61</td>
-                <td>2011/04/25</td>
-                <td>$320,800</td>
-            </tr>
-            
-        </tbody>
-        <tfoot>
-            <tr>
-                <th>Name</th>
-                <th>Position</th>
-                <th>Office</th>
-                <th>Age</th>
-                <th>Start date</th>
-                <th>Salary</th>
-            </tr>
-        </tfoot>
-    </table>
-                              <!-- </div>
+						       </div>
                               </div>
 						</div>
                     </div>
-				</form>	 -->
+				</form>	 
 				
 			
 				<div class="panel panel-default height">
@@ -81,29 +41,6 @@ $bill          = $this->db->where(array('bill_id' => $parm,'ledger_type' => 'buy
 			
 
 </div>
-<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-<script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
-<script src="https://cdn.datatables.net/1.10.16/js/dataTables.bootstrap.min.js"></script>
 
 
 
-<script type="text/javascript">
-
-var table;
-$(document).ready(function() {
-    $('#datatable').DataTable( );
-} );$(document).ready(function() {
-    var eventFired = function ( type ) {
-        var n = $('#demo_info')[0];
-        n.innerHTML += '<div>'+type+' event - '+new Date().getTime()+'</div>';
-        n.scrollTop = n.scrollHeight;      
-    }
- 
-    $('#example')
-        .on( 'order.dt',  function () { eventFired( 'Order' ); } )
-        .on( 'search.dt', function () { eventFired( 'Search' ); } )
-        .on( 'page.dt',   function () { eventFired( 'Page' ); } )
-        .DataTable();
-} );
-
-</script>
